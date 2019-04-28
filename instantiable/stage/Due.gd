0e445ee6e3ec9aed6d763b729e3de81e90c2decb
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-04-28 20:09:28 +0100

    added monitor to scene plus some animations

--- instantiable/stage/Due.gd
+++ instantiable/stage/Due.gd
@@ -5,10 +5,11 @@ export (int) var amount = 0 setget set_amount
 
 func set_amount(i):
 	amount = i
-	if amount < 0:
-		$Label.text = "Change Due:"
-	else:
-		$Label.text = "Payment Due:"
-	$Money.text = "$" + str(abs(amount))
+	if $Label:
+		if amount < 0:
+			$Label.text = "Change Due:"
+		else:
+			$Label.text = "Payment Due:"
+		$Money.text = "$" + str(abs(amount))
 
 
