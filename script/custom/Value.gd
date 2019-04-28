15e88e945aa4aab1605bd2f71e2a1c546542f76c
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-04-28 09:47:28 +0100

    Added ability to select predefined in Value node
    added support for selecting values from a global enum, this should make it a lot easier to keep track of different currency types and make changes to them if needed

--- script/custom/Value.gd
+++ script/custom/Value.gd
@@ -1,5 +1,29 @@
+tool
 extends Node
 
 class_name Value, "res://image/dodlar_sign.svg"
-export (int) var amount = 1
+export (int) var amount = 1 setget set_amount
+export (int, 0, 5) var fixed_amount = null setget set_fixed_amount
+
+func set_amount(i, update_fixed=true):
+	amount = i
+	if update_fixed:
+		for k in Utils.MONEY.keys():
+			if amount == Utils.MONEY[k]:
+				set_fixed_amount(k, false)
+				return
+		set_fixed_amount(null, false)
+	else:
+		property_list_changed_notify()
+
+func set_fixed_amount(i, update_amount=true):
+	fixed_amount = i
+	if update_amount:
+		set_amount(Utils.MONEY[i], false)
+		print(amount)
+	else:
+		property_list_changed_notify()
+
+
+
 
